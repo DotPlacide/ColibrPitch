00bcdc336f4ea5921ca20afdc74a1032b299169f
DotPlacide <jean.boungou@melwater.org>
2020-03-20 14:28:35 +0000

    build

---
+++ build/precache-manifest.95692295131964ecfdeacd369c772a1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7c6d2b53c65e4fff84e500cf8914cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c16a596e3b771cdf32",
+    "url": "/static/css/main.8af6b214.chunk.css"
+  },
+  {
+    "revision": "01c5aad59bb55b9ea353",
+    "url": "/static/js/2.b3b95374.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3b95374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c16a596e3b771cdf32",
+    "url": "/static/js/main.91cccc1c.chunk.js"
+  },
+  {
+    "revision": "69c851d5bf034a239616",
+    "url": "/static/js/runtime-main.a8e065c7.js"
+  },
+  {
+    "revision": "9adfb4d37ea55621c4c5593a1209970d",
+    "url": "/static/media/gb2.9adfb4d3.png"
+  },
+  {
+    "revision": "2d5bd374927c6ea17423ec94a561c832",
+    "url": "/static/media/logo.2d5bd374.png"
+  },
+  {
+    "revision": "38db5e79c96aaa626e569a0aa6b0c32f",
+    "url": "/static/media/test.38db5e79.jpg"
+  }
+]);
